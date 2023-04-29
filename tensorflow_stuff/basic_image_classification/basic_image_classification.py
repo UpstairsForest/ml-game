048778b788e82abc577240bfaf8852ac6bdb1852
UpstairsForest <nikita.batalov@uzh.ch>
2023-04-29 05:46:22 +0200

    save weights and plot accuracy evolution

--- tensorflow_stuff/basic_image_classification/basic_image_classification.py
+++ tensorflow_stuff/basic_image_classification/basic_image_classification.py
@@ -42,10 +42,23 @@ model.compile(
     metrics=["accuracy"],
 )
 
-epochs = 10
-for e in range(epochs):
-    model.fit(train_images, train_labels, epochs=1, verbose=1)
-    test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+checkpoint_path = "./checkpoints/cp-{epoch:04d}.ckpt"
+cp_callback = tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_path,
+    verbose=1,
+    save_weights_only=True,
+)
+model.save_weights(
+    checkpoint_path.format(epoch=0)
+)  # formatting to save multiple checkpoints
+mega_history = model.fit(
+    train_images,
+    train_labels,
+    epochs=30,
+    verbose=1,
+    validation_data=(test_images, test_labels),
+    callbacks=[cp_callback],
+)
 
 # Predictions
 prob_model = tf.keras.Sequential(
@@ -56,48 +69,58 @@ prob_model = tf.keras.Sequential(
 )
 predictions = prob_model.predict(test_images)
 
+#
+# # Plot predictions
+# def plot_image(i, predictions_array, true_label, img):
+#     true_label, img = true_label[i], img[i]
+#     plt.grid(False)
+#     plt.xticks([])
+#     plt.yticks([])
+#
+#     plt.imshow(img, cmap=plt.cm.binary)
+#
+#     predicted_label = np.argmax(predictions_array)
+#     if predicted_label == true_label:
+#         color = "blue"
+#     else:
+#         color = "red"
+#
+#     plt.xlabel(
+#         "{} {:2.0f}% ({})".format(
+#             class_names[predicted_label],
+#             100 * np.max(predictions_array),
+#             class_names[true_label],
+#         ),
+#         color=color,
+#     )
+#
+#
+# def plot_value_array(i, predictions_array, true_label):
+#     true_label = true_label[i]
+#     plt.grid(False)
+#     plt.xticks(range(10))
+#     plt.yticks([])
+#     this_plot = plt.bar(range(10), predictions_array, color="#777777")
+#     plt.ylim([0, 1])
+#     predicted_label = np.argmax(predictions_array)
+#
+#     this_plot[predicted_label].set_color("red")
+#     this_plot[true_label].set_color("blue")
+#
+#
+# image_index = np.random.choice([i for i in range(len(test_images))])
+# plt.figure(figsize=(6, 3))
+# plt.subplot(1, 2, 1)
+# plot_image(image_index, predictions[image_index], test_labels, test_images)
+# plt.subplot(1, 2, 2)
+# plot_value_array(image_index, predictions[image_index], test_labels)
+# plt.show()
 
-def plot_image(i, predictions_array, true_label, img):
-    true_label, img = true_label[i], img[i]
-    plt.grid(False)
-    plt.xticks([])
-    plt.yticks([])
-
-    plt.imshow(img, cmap=plt.cm.binary)
-
-    predicted_label = np.argmax(predictions_array)
-    if predicted_label == true_label:
-        color = "blue"
-    else:
-        color = "red"
-
-    plt.xlabel(
-        "{} {:2.0f}% ({})".format(
-            class_names[predicted_label],
-            100 * np.max(predictions_array),
-            class_names[true_label],
-        ),
-        color=color,
-    )
-
-
-def plot_value_array(i, predictions_array, true_label):
-    true_label = true_label[i]
-    plt.grid(False)
-    plt.xticks(range(10))
-    plt.yticks([])
-    this_plot = plt.bar(range(10), predictions_array, color="#777777")
-    plt.ylim([0, 1])
-    predicted_label = np.argmax(predictions_array)
-
-    this_plot[predicted_label].set_color("red")
-    this_plot[true_label].set_color("blue")
-
-
-i = np.random.choice([i for i in range(len(test_images))])
-plt.figure(figsize=(6, 3))
-plt.subplot(1, 2, 1)
-plot_image(i, predictions[i], test_labels, test_images)
-plt.subplot(1, 2, 2)
-plot_value_array(i, predictions[i], test_labels)
+# Plot history
+fig, ax = plt.subplots()
+ax.plot(mega_history.epoch, mega_history.history["accuracy"], color="cyan", label="training")
+ax.plot(mega_history.epoch, mega_history.history["val_accuracy"], color="orange", label="validation")
+ax.set(xlabel="epoch", ylabel="accuracy")
+ax.grid()
+ax.legend()
 plt.show()

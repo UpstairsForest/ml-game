beb3d07c4616503750c0092abc0da34ec85c6a08
UpstairsForest <nikita.batalov@uzh.ch>
2023-04-29 04:55:18 +0200

    black

--- tensorflow_stuff/basic_image_classification/basic_image_classification.py
+++ tensorflow_stuff/basic_image_classification/basic_image_classification.py
@@ -7,23 +7,39 @@ import matplotlib.pyplot as plt
 # Data
 fashion_mnist = tf.keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+class_names = [
+    "T-shirt/top",
+    "Trouser",
+    "Pullover",
+    "Dress",
+    "Coat",
+    "Sandal",
+    "Shirt",
+    "Sneaker",
+    "Bag",
+    "Ankle boot",
+]
 train_images, test_images = train_images / 255.0, test_images / 255.0
 
 # Model setup
-model = tf.keras.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28, 28)),  # flatten inputs from 28, 28 to 784
-    tf.keras.layers.Dense(128, activation="relu"),
-    tf.keras.layers.Dense(10),  # every input is connected to every output by weights
-])
+model = tf.keras.Sequential(
+    [
+        tf.keras.layers.Flatten(
+            input_shape=(28, 28)
+        ),  # flatten inputs from 28, 28 to 784
+        tf.keras.layers.Dense(128, activation="relu"),
+        tf.keras.layers.Dense(
+            10
+        ),  # every input is connected to every output by weights
+    ]
+)
 model.compile(
     # measures how accurate the model is during training, loss is minimized
     loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
     # determines the model is updated based on data it sees and its loss function
     optimizer="adam",
     # for monitoring the training
-    metrics=["accuracy"]
+    metrics=["accuracy"],
 )
 
 epochs = 10
@@ -32,10 +48,12 @@ for e in range(epochs):
     test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
 
 # Predictions
-prob_model = tf.keras.Sequential([
-    model,  # keeps the weights
-    tf.keras.layers.Softmax(),  # turn whatever previous layer outputs into nice probabilities
-])
+prob_model = tf.keras.Sequential(
+    [
+        model,  # keeps the weights
+        tf.keras.layers.Softmax(),  # turn whatever previous layer outputs into nice probabilities
+    ]
+)
 predictions = prob_model.predict(test_images)
 
 
@@ -49,14 +67,18 @@ def plot_image(i, predictions_array, true_label, img):
 
     predicted_label = np.argmax(predictions_array)
     if predicted_label == true_label:
-        color = 'blue'
+        color = "blue"
     else:
-        color = 'red'
+        color = "red"
 
-    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
-                                         100 * np.max(predictions_array),
-                                         class_names[true_label]),
-               color=color)
+    plt.xlabel(
+        "{} {:2.0f}% ({})".format(
+            class_names[predicted_label],
+            100 * np.max(predictions_array),
+            class_names[true_label],
+        ),
+        color=color,
+    )
 
 
 def plot_value_array(i, predictions_array, true_label):
@@ -68,8 +90,8 @@ def plot_value_array(i, predictions_array, true_label):
     plt.ylim([0, 1])
     predicted_label = np.argmax(predictions_array)
 
-    this_plot[predicted_label].set_color('red')
-    this_plot[true_label].set_color('blue')
+    this_plot[predicted_label].set_color("red")
+    this_plot[true_label].set_color("blue")
 
 
 i = np.random.choice([i for i in range(len(test_images))])

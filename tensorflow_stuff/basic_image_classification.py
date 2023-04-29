426cd444662ee14cacb1a0da36b1dba0dede5ff3
UpstairsForest <nikita.batalov@uzh.ch>
2023-04-29 04:52:17 +0200

    finish basic image classification example

--- tensorflow_stuff/basic_image_classification.py
+++ tensorflow_stuff/basic_image_classification.py
@@ -26,8 +26,56 @@ model.compile(
     metrics=["accuracy"]
 )
 
-# Train
-model.fit(train_images, train_labels, epochs=20, verbose=1)
+epochs = 10
+for e in range(epochs):
+    model.fit(train_images, train_labels, epochs=1, verbose=1)
+    test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
 
-# Evaluate
-test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+# Predictions
+prob_model = tf.keras.Sequential([
+    model,  # keeps the weights
+    tf.keras.layers.Softmax(),  # turn whatever previous layer outputs into nice probabilities
+])
+predictions = prob_model.predict(test_images)
+
+
+def plot_image(i, predictions_array, true_label, img):
+    true_label, img = true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img, cmap=plt.cm.binary)
+
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                         100 * np.max(predictions_array),
+                                         class_names[true_label]),
+               color=color)
+
+
+def plot_value_array(i, predictions_array, true_label):
+    true_label = true_label[i]
+    plt.grid(False)
+    plt.xticks(range(10))
+    plt.yticks([])
+    this_plot = plt.bar(range(10), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    this_plot[predicted_label].set_color('red')
+    this_plot[true_label].set_color('blue')
+
+
+i = np.random.choice([i for i in range(len(test_images))])
+plt.figure(figsize=(6, 3))
+plt.subplot(1, 2, 1)
+plot_image(i, predictions[i], test_labels, test_images)
+plt.subplot(1, 2, 2)
+plot_value_array(i, predictions[i], test_labels)
+plt.show()

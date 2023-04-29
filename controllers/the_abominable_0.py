fix direction calculation and saving weights

controllers/the_abominable_0.py
@@ -15,7 +15,9 @@ class TheAbominable0(LWalk):
 
     model: tf.keras.Model
     board_manager: BoardManager
-    checkpoint_path = "../fixtures/checkpoints/the_abominable_0/cp.ckpt"
+    print(os.getcwd())
+    checkpoint_path = os.path.join(os.getcwd(), "fixtures/checkpoints/the_abominable_0/cp.ckpt")
+    print(checkpoint_path)
 
     def __init__(self, board_manager: BoardManager):
         # it's 'abominable' for a reason
@@ -81,10 +83,11 @@ class TheAbominable0(LWalk):
         )
 
         # fit
-        self.model.fit(data, goal, epochs=3, verbose=1, callbacks=cp_callback)
+        self.model.fit(data, goal, epochs=3, verbose=0, callbacks=[cp_callback])
         # predict
         prediction_model = tf.keras.Sequential([self.model, tf.keras.layers.Softmax()])
-        predicted_move: Move = moves[np.argmax(prediction_model.predict(data))]
+        prediction = prediction_model.predict(data, verbose=0)
+        predicted_move: Move = moves[np.argmax(prediction)]
 
         # update controller-related fields
         self.current_position = predicted_move.end

game/logic.py
@@ -45,9 +45,8 @@ def rate_move(move: Move, current_board: Board):
     if not x or not y:
         raise Exception("failed to find game_end")
     # scalar product
-    prod = np.dot(
-        [move.end.x - move.start.x, move.end.y - move.start.y],  # actor_start to actor_end vector
-        [x - move.start.x, y - move.start.y],  # actor_start to game_end vector
-    )
+    v1 = [move.end.x - move.start.x, move.end.y - move.start.y]  # actor_start to actor_end vector
+    v2 = [x - move.start.x, y - move.start.y]  # actor_start to game_end vector
+    prod = np.dot(v1, v2)
     # normalize
-    return prod / np.abs(prod)
+    return prod / (np.linalg.norm(v1) * np.linalg.norm(v2))